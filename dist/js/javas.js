bb7eab3b5133eb0d983d6ea68000b911a6fb1260
Mohammed Fathi <30496179+M7mdHendawy@users.noreply.github.com>
2018-12-25 16:22:22 +0200

    upload

---
+++ dist/js/javas.js
@@ -0,0 +1,12 @@
+$('.slider-component').slick({
+    rtl: false,
+    autoplay: false,
+    autoplaySpeed: 5000,
+    speed: 500,
+    dots: false,
+    arrows:true,
+    centerMode:true,
+    prevArrow: '<a href="javascript:void(0)" class="slick-prev ti-arrow-left-chevron"></a>',
+    nextArrow: '<a href="javascript:void(0)" class="slick-next ti-arrow-right-chevron"></a>',
+});
+
